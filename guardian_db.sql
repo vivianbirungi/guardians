724454f0bb51d33ab3ec2894ba052b67011f7afb
kigh143 <ghkatende@gmail.com>
2018-09-14 14:41:04 +0300

    added the database

--- guardian_db.sql
+++ guardian_db.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 14, 2018 at 09:00 
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.20
+-- Generation Time: Sep 14, 2018 at 01:40 PM
+-- Server version: 10.1.33-MariaDB
+-- PHP Version: 7.2.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,7 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `portifolio` (
   `portifolio_id` int(30) NOT NULL,
-  `portifolio_name` varchar(30) NOT NULL,
+  `portifolio_name` varchar(230) NOT NULL,
   `portifolio_image` varchar(150) NOT NULL,
   `portifolio_description` text NOT NULL,
   `date_joined` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -40,18 +42,11 @@ CREATE TABLE `portifolio` (
 --
 
 INSERT INTO `portifolio` (`portifolio_id`, `portifolio_name`, `portifolio_image`, `portifolio_description`, `date_joined`, `category`) VALUES
-(1, 'fish-go', 'img11.jpg', 'fish is good. Have it make it very nice and enjoy with onions, blue water and oil.', '2018-08-07 15:42:25', 'Fishery'),
-(2, 'meat-go', 'img12.jpg', 'fish is good. Have it make it very nice and enjoy with onions, blue water and oil.', '2018-08-07 15:42:25', 'Fishery'),
-(3, 'tuna', 'img13.jpg', 'fish is good. Have it make it very nice and enjoy with onions, blue water and oil.', '2018-08-07 15:42:25', 'Fishery'),
-(4, 'heder twi', 'img12.jpg', 'fish is good. Have it make it very nice and enjoy with onions, blue water and oil.', '2018-08-07 15:42:25', 'Educ'),
-(5, 'heswdwerr ', 'img13.jpg', 'fish is good. Have it make it very nice and enjoy with onions, blue water and oil.', '2018-08-07 15:42:25', 'Educ'),
-(6, 'header two', 'img13.jpg', 'fish is good. Have it make it very nice and enjoy with onions, blue water and oil.', '2018-08-07 15:42:25', 'Educ'),
-(7, 'movies', 'img11.jpg', 'fish is good. Have it make it very nice and enjoy with onions, blue water and oil.', '2018-08-07 15:42:25', 'philosp'),
-(8, 'huni', 'img12.jpg', 'fish is good. Have it make it very nice and enjoy with onions, blue water and oil.', '2018-08-07 15:42:25', 'philosp'),
-(9, 'biutv', 'img13.jpg', 'fish is good. Have it make it very nice and enjoy with onions, blue water and oil.', '2018-08-07 15:42:25', 'philosp'),
-(10, 'making it', 'img13.jpg', 'fish is good. Have it make it very nice and enjoy with onions, blue water and oil.', '2018-08-07 15:42:25', 'market'),
-(11, 'market search', 'img11.jpg', 'fish is good. Have it make it very nice and enjoy with onions, blue water and oil.', '2018-08-07 15:42:25', 'market'),
-(12, 'marketo', 'img11.jpg', 'fish is good. Have it make it very nice and enjoy with onions, blue water and oil.', '2018-08-07 15:42:25', 'market');
+(1, 'Ugachick Aquafeed Market Survey in East Africa', 'img11.jpg', 'fish is good. Have it make it very nice and enjoy with onions, blue water and oil.', '2018-08-07 15:42:25', 'Fishery'),
+(2, 'Private Sector Foundation Uganda (PSFU)', 'img12.jpg', 'fish is good. Have it make it very nice and enjoy with onions, blue water and oil.', '2018-08-07 15:42:25', 'Fishery'),
+(3, 'Matching Grant Reg. No. MGF/FSH/CP4/0002', 'img13.jpg', 'fish is good. Have it make it very nice and enjoy with onions, blue water and oil.', '2018-08-07 15:42:25', 'Fishery'),
+(4, 'School-based Project aiming at Conserving Lake Victoria ', 'img12.jpg', 'School-based Project aiming at Conserving Lake Victoria through Performing Arts and Tree Planting Campaigns. Funded by World Bank through Lake Victoria Environmental Management Project (LVEMP II)', '2018-08-07 15:42:25', 'Educ'),
+(5, 'bioremediation of the aquaculture effluents', 'img13.jpg', 'Use of constructed wetlands concept in bioremediation of the aquaculture effluents from Source of the Nile Fish Farm, Buikwe district ', '2018-08-07 15:42:25', 'Educ');
 
 --
 -- Indexes for dumped tables
@@ -72,6 +67,8 @@ ALTER TABLE `portifolio`
 --
 ALTER TABLE `portifolio`
   MODIFY `portifolio_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
